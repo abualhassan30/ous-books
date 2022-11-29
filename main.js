d0fc1454993b9d2b7f08f5ea249be8045c29747b
Gedo <43795422+abualhassan30@users.noreply.github.com>
2022-11-29 13:14:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+    var jobCount = $('.list-group h2').length;
+    $('.list-count').text(jobCount + ' items');
+      
+    
+    $("#search-text").keyup(function () {
+       //$(this).addClass('hidden');
+    
+      var searchTerm = $("#search-text").val();
+      var listItem = $('.list-group').children('li');
+    
+      
+      var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+      
+        //extends :contains to be case insensitive
+    $.extend($.expr[':'], {
+    'containsi': function(elem, i, match, array)
+    {
+      return (elem.textContent || elem.innerText || '').toLowerCase()
+      .indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+      
+      
+      $(".list-group li").not(":containsi('" + searchSplit + "')").each(function(e)   {
+        $(this).addClass('hiding out').removeClass('in');
+        setTimeout(function() {
+            $('.out').addClass('hidden');
+          }, 300);
+      });
+      
+      $(".list-group h2:containsi('" + searchSplit + "')").each(function(e) {
+        $(this).removeClass('hidden out').addClass('in');
+        setTimeout(function() {
+            $('.in').removeClass('hiding');
+          }, 1);
+      });
+      
+    
+        var jobCount = $('.list-group h2').length;
+      $('.list-count').text(jobCount + ' items');
+      
+      //shows empty state text when no jobs found
+      if(jobCount == '0') {
+        $('#list').addClass('empty');
+      }
+      else {
+        $('.list-group').removeClass('empty');
+      }
+      
+    });
+  
+    
+                    
+       /*  
+       An extra! This function implements
+       jQuery autocomplete in the searchbox.
+       Uncomment to use :)
+       
+       
+   function searchList() {                
+      //array of list items
+      var listArray = [];
+    
+       $("#list li").each(function() {
+      var listText = $(this).text().trim();
+        listArray.push(listText);
+      });
+      
+      $('#search-text').autocomplete({
+          source: listArray
+      });
+      
+      
+    }
+                                     
+    searchList();
+  */
+    
+                      
+  });
+  
+  
+  
+  
+  
+  
+  
+  
